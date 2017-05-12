b4fe588f0358c3be3d678b8d10a39ff628a8f251
James D Stewart Jr <jamstewjr@gmail.com>
2017-05-12 13:11:38 -0700

    Create tests and functions of creating tasks and getting task

--- models/database.js
+++ models/database.js
@@ -1,1 +1,40 @@
-var pgp = require('pg-promise')
+var pgp = require('pg-promise')()
+var db = pgp({ database: 'secure-to-do' })
+
+function createTask (title) {
+  return db.one('INSERT INTO tasks(title) VALUES ($1) RETURNING *',
+    title)
+  .then(function (data) {
+    console.log(data)
+    return ('Task created.')
+  })
+  .catch(function (error) {
+    console.log(error)
+    return error
+  })
+}
+
+function getAllTask () {
+  return db.many('SELECT * FROM tasks')
+  .then(function (data) {
+    return data
+  })
+  .catch(function (error) {
+    return error
+  })
+}
+
+function editTaskById () {
+  return 3
+}
+
+function deleteTaskById () {
+  return 4
+}
+
+module.exports = {
+  createTask: createTask,
+  getAllTask: getAllTask,
+  editTaskById: editTaskById,
+  deleteTaskById: deleteTaskById
+}
--- test/database-spec.js
+++ test/database-spec.js
@@ -1,10 +1,46 @@
-var expect = require('chai').expect
+var chai = require('chai')
+var expect = chai.expect
+var chaiChange = require('chai-change')
 var db = require('../models/database.js')
 
-describe('Test', function () {
-  context('test()', function () {
-    it('should exist', function () {
-      expect(test).to.exist
+chai.use(chaiChange)
+
+describe('Database', function () {
+  var dbb
+  beforeEach('invokes a new database before each test', function () {
+    dbb = db
+  })
+
+  context('createTask()', function () {
+    it('should insert a new task into the database', function (done) {
+      dbb.createTask('And another 1')
+        .then(function (data) {
+          expect(data).to.equal('Task created.')
+          done()
+        })
+    })
+  })
+
+  context('getAllTask()', function () {
+    it('should return all tasks from the database', function (done) {
+      dbb.getAllTask()
+        .then(function (data) {
+          console.log(data)
+          expect(data).to.equal(data)
+          done()
+        })
+    })
+  })
+
+  context('editTaskById()', function () {
+    it('should edit a task in the database', function () {
+      return expect(dbb.editTaskById('1')).to.equal('Task created.')
+    })
+  })
+
+  context('deleteTaskById()', function () {
+    it('should delete a task from the database', function () {
+      return expect(dbb.deleteTaskById('And another 1')).to.equal('Task created.')
     })
   })
 })
